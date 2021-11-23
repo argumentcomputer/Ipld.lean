324352c269706584149496c6d3157ec51fd82fb4
Anders Christiansen Sørby <anders@sorby.xyz>
2021-11-23 12:10:14 +0100

    Add toByteArrayBE test in main

--- Ipld.lean
+++ Ipld.lean
@@ -5,5 +5,7 @@ import Ipld.Cid
 import Ipld.Ipld
 import Blake3
 
-def main : IO Unit :=
+def main : IO Unit := do
   println! "Hello, world! {Blake3.hash "hello".toByteArray}"
+  let x := Nat.toByteArrayBE 3
+  println! "test {Blake3.hash x}"
