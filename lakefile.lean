6a76e15599b55aace6b7ba605055d52a834e8011
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-05-31 19:51:21 -0300

    update LSpec dependency

--- lakefile.lean
+++ lakefile.lean
@@ -6,6 +6,6 @@ package Ipld {
   defaultFacet := PackageFacet.oleans
   dependencies := #[{
     name := `LSpec
-    src := Source.git "https://github.com/yatima-inc/LSpec.git" "small-simplifications"
+    src := Source.git "https://github.com/yatima-inc/LSpec.git" "7525505db2172f6ee48e0e890eab46907cf3d9f6"
   }]
 }
