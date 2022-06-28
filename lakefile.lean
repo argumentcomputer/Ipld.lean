dc28f56530dbaae81f38267e81069a588bfc4918
Hanting Zhang <hantingz@usc.edu>
2022-06-28 14:35:50 -0700

    bump lspec

--- lakefile.lean
+++ lakefile.lean
@@ -6,6 +6,6 @@ package Ipld {
   defaultFacet := PackageFacet.oleans
   dependencies := #[{
     name := `LSpec
-    src := Source.git "https://github.com/yatima-inc/LSpec.git" "f1497c32efdaab4c707b86caa688ab5efeb10110"
+    src := Source.git "https://github.com/yatima-inc/LSpec.git" "04df8a2b03c7d1e2ff24eb14eed44c0a82218863"
   }]
 }
