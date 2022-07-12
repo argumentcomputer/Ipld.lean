9fe624483318a45d203614b7cba5ca2261dff0ab
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-12 09:52:19 -0300

    fix LSpec integration

--- lakefile.lean
+++ lakefile.lean
@@ -7,12 +7,12 @@ package Ipld
 lean_lib Ipld
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "95b36c3a13e32355a9222e1dad33e354c604798d"
+  "https://github.com/yatima-inc/LSpec.git" @ "70e792016f8e2682fbcf65624708c85ceaf8db63"
 
 require YatimaStdLib from git
   "https://github.com/yatima-inc/YatimaStdLib.lean" @ "35aecd8951778f45a47d12376635c26a815dcb25"
 
-lean_exe Tests.DagCBor
+lean_exe Tests.DagCbor
 lean_exe Tests.Multibase
 lean_exe Tests.Multihash
 lean_exe Tests.UnsignedVarInt
