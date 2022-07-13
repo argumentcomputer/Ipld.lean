75a1566155aa650079c21bdb38828ef6d0f4180c
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-13 19:45:59 -0300

    bumping lspec and stdlib versions

--- lakefile.lean
+++ lakefile.lean
@@ -7,10 +7,10 @@ package Ipld
 lean_lib Ipld
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "70e792016f8e2682fbcf65624708c85ceaf8db63"
+  "https://github.com/yatima-inc/LSpec.git" @ "0222cb5a6543283dc3c40c7ccd401cb54609d3d0"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "35aecd8951778f45a47d12376635c26a815dcb25"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "80b290a322267aee7dbca96b2547fa24de64236a"
 
 lean_exe Tests.DagCbor
 lean_exe Tests.Multibase
