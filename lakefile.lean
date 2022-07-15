eb09098e19a046b0fc16ad55ff047993d55b0173
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-07-15 18:30:58 -0300

    update lspec version

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package Ipld
 lean_lib Ipld
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "95f4f5b865a5ac8044fc3c9c2c83a7220f806d4c"
+  "https://github.com/yatima-inc/LSpec.git" @ "3b759f6e7798fdb6b17ae83ea060cd34e89b7e91"
 
 require YatimaStdLib from git
   "https://github.com/yatima-inc/YatimaStdLib.lean" @ "80b290a322267aee7dbca96b2547fa24de64236a"
