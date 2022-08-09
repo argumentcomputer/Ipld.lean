98f1abf73584ddb06e6e42f5fdfb835e324b831e
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-08-09 18:08:09 -0300

    bump LSpec

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package Ipld
 lean_lib Ipld
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "77fc51697abeff937ffd20d2050723dc0fa1c8c0"
+  "https://github.com/yatima-inc/LSpec.git" @ "59de5823f0f055f06cacd7c066fff6c3e14b13a2"
 
 require YatimaStdLib from git
   "https://github.com/yatima-inc/YatimaStdLib.lean" @ "a2bbc9a48db7efd5d761a5b27f2cc6c1863b9622"
