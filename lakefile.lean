b25fd1c808a7af17dd02145850f41ecc88748966
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-09-12 13:26:12 -0300

    update lake hashes

--- lakefile.lean
+++ lakefile.lean
@@ -7,10 +7,10 @@ package Ipld
 lean_lib Ipld
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "7e2d41644519e8c437fbe7461544eaa855738f73"
+  "https://github.com/yatima-inc/LSpec.git" @ "c63610bb23451c7aa2faae17c71e8d162c6c616e"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "9c362443e0d89eb96683b52a1caaf762049697c4"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "dd565ffec739f9ee0a79a3bf47ab5e1e0db0d8e2"
 
 lean_exe Tests.DagCbor
 lean_exe Tests.Multibase
