4648d21bc025bdede3b2a581859618010aeb7ad2
Arthur Paulino <arthurleonardo.ap@gmail.com>
2022-09-13 10:53:50 -0300

    update CI, LSpec hash and manifest

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ package Ipld
 lean_lib Ipld
 
 require LSpec from git
-  "https://github.com/yatima-inc/LSpec.git" @ "c63610bb23451c7aa2faae17c71e8d162c6c616e"
+  "https://github.com/yatima-inc/LSpec.git" @ "a8dc2f38fc38f16efcc877ca8a4c7b37d3965db0"
 
 require YatimaStdLib from git
   "https://github.com/yatima-inc/YatimaStdLib.lean" @ "dd565ffec739f9ee0a79a3bf47ab5e1e0db0d8e2"
