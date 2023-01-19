716e787eba461dba1c5b9bb9977147564865309d
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-01-19 12:30:09 -0300

    bump libs

--- lakefile.lean
+++ lakefile.lean
@@ -11,7 +11,7 @@ require LSpec from git
   "https://github.com/yatima-inc/LSpec.git" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "704823e421b333ea9960347e305c60f654618422"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "649368d593f292227ab39b9fd08f6a448770dca8"
 
 lean_exe Tests.DagCbor
 lean_exe Tests.Multibase
